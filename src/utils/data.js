0dcbde6f9dc1b358cba056945a112b847fbb51e2
Waleed Ajmal <127334056+Majboor@users.noreply.github.com>
2023-10-07 21:15:39 +0500

    update to SMMA courses

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,164 +1,102 @@
 import { course_images } from "./images";
 
  //  category names 
-export const PYTHON = "Islamic Studies";
-export const WEB_DEVELOPMENT = "Islamic History";
-export const DATA_SCIENCE = "Quranic Studies";
-export const AWS = "Islamic Law";
-export const DESIGN = "Islamic Art";
-export const MARKETING = "Islamic Finance";
+// Updated category names
+export const PYTHON = "Social Media Marketing";
+export const WEB_DEVELOPMENT = "Social Media Marketing History";
+export const DATA_SCIENCE = "Social Media Marketing Studies";
+export const AWS = "Social Media Marketing Law";
+export const DESIGN = "Social Media Marketing Art";
+export const MARKETING = "Social Media Marketing Finance";
 
-//courses
 const courses = [
-    
     {
         id: "xMHCZl",
-        category: "Islamic Studies", // Change category
+        category: "Social Media Marketing", // Change category
         image: course_images.python_5,
-        course_name: "Islamic Studies: The Comprehensive Islamic Course",
-        description: "Learn A-Z everything about Islamic Studies, from the basics, to advanced topics in Quran, Hadith, and more!",
+        course_name: "Social Media Marketing: The Comprehensive Course",
+        description: "Learn A-Z everything about Social Media Marketing, from the basics to advanced topics in digital marketing and social media strategies!",
         rating_count: 3059,
         rating_star: 4.4,
         students: 18430,
-        creator: "Islamic Scholar Institute",
+        creator: "Social Media Marketing Institute",
         updated_date: "9/2015",
         lang: "English",
         actual_price: 84.99,
         discounted_price: 9.99,
         what_you_will_learn: [
-            "Study Islamic Theology",
-            "Become Proficient in Quranic Recitation",
-            "Understand Hadith Interpretation"
+            "Study Social Media Marketing Theories",
+            "Become Proficient in Social Media Advertising",
+            "Understand Social Media Analytics"
         ],
         content: [
-            "Introduction to Islamic Studies",
-            "The Quran and Its Sciences",
-            "Hadith and Its Importance",
-            "Islamic Jurisprudence",
-            "Islamic History",
-            "Islamic Ethics",
-            "Contemporary Issues in Islam"
+            "Introduction to Social Media Marketing",
+            "Digital Marketing Strategies",
+            "Social Media Advertising",
+            "Social Media Analytics",
+            "Social Media History",
+            "Social Media Ethics",
+            "Contemporary Issues in Social Media Marketing"
         ]
     },
     {
         id: "dtfo9e",
-        category: "Islamic Studies", // Change category
+        category: "Social Media Marketing", // Change category
         image: course_images.python_4,
-        course_name: "Islamic Finance and Banking",
-        description: "Learn about Islamic finance principles and banking practices in accordance with Shariah law.",
+        course_name: "Social Media Marketing Finance and Banking",
+        description: "Learn about financial principles and banking practices in the context of Social Media Marketing.",
         rating_count: 3059,
         rating_star: 4.3,
         students: 184053,
-        creator: "Islamic Finance Expert",
+        creator: "Social Media Marketing Finance Expert",
         updated_date: "9/2019",
         lang: "English",
         actual_price: 84.99,
         discounted_price: 9.99,
         what_you_will_learn: [
-            "Understand the Principles of Islamic Finance",
-            "Explore Shariah-Compliant Investment",
-            "Learn Islamic Banking Practices",
-            "Evaluate Islamic Insurance (Takaful)"
+            "Understand Financial Principles in Social Media Marketing",
+            "Explore Social Media Marketing Investment",
+            "Learn Social Media Banking Practices",
+            "Evaluate Social Media Marketing Insurance"
         ],
         content: [
-            "Introduction to Islamic Finance",
-            "Islamic Banking Products",
-            "Islamic Investment Principles",
-            "Islamic Insurance (Takaful)",
-            "Islamic Financial Markets",
-            "Islamic Finance Case Studies"
+            "Introduction to Financial Principles in Social Media Marketing",
+            "Social Media Banking Products",
+            "Social Media Marketing Investment Principles",
+            "Social Media Marketing Insurance",
+            "Social Media Financial Markets",
+            "Social Media Marketing Finance Case Studies"
         ]
     },
     {
         id: "fLroW6",
-        category: "Islamic Studies", // Change category
+        category: "Social Media Marketing", // Change category
         image: course_images.python_3,
-        course_name: "Islamic Art and Architecture",
-        description: "Explore the rich heritage of Islamic art and architecture across different cultures and time periods.",
+        course_name: "Social Media Marketing Art and Design",
+        description: "Explore the art and design aspects of Social Media Marketing across different platforms and creative strategies.",
         rating_count: 1844,
         rating_star: 4.3,
         students: 7330,
-        creator: "Islamic Art Historian",
+        creator: "Social Media Marketing Art Historian",
         updated_date: "5/2022",
         lang: "English",
         actual_price: 84.99,
         discounted_price: 9.99,
         what_you_will_learn: [
-            "Discover Islamic Art Styles",
-            "Explore Islamic Architecture",
-            "Understand Calligraphy and Manuscripts"
+            "Discover Social Media Art Styles",
+            "Explore Social Media Design",
+            "Understand Calligraphy and Creativity in Social Media"
         ],
         content: [
-            "Introduction to Islamic Art",
-            "Islamic Calligraphy",
-            "Islamic Manuscripts",
-            "Islamic Architecture",
-            "Islamic Art Across Cultures",
-            "Islamic Art Conservation"
+            "Introduction to Art in Social Media Marketing",
+            "Social Media Calligraphy",
+            "Social Media Art and Design",
+            "Social Media Art Across Platforms",
+            "Social Media Art Conservation"
         ]
     },
-    
-
-    {
-        id: "dtfo9e",
-        category: "Islamic History", // Change category
-        image: course_images.python_4,
-        course_name: "Islamic Finance and Banking",
-        description: "Learn about Islamic finance principles and banking practices in accordance with Shariah law.",
-        rating_count: 3059,
-        rating_star: 4.3,
-        students: 184053,
-        creator: "Islamic Finance Expert",
-        updated_date: "9/2019",
-        lang: "English",
-        actual_price: 84.99,
-        discounted_price: 9.99,
-        what_you_will_learn: [
-            "Understand the Principles of Islamic Finance",
-            "Explore Shariah-Compliant Investment",
-            "Learn Islamic Banking Practices",
-            "Evaluate Islamic Insurance (Takaful)"
-        ],
-        content: [
-            "Introduction to Islamic Finance",
-            "Islamic Banking Products",
-            "Islamic Investment Principles",
-            "Islamic Insurance (Takaful)",
-            "Islamic Financial Markets",
-            "Islamic Finance Case Studies"
-        ]
-    },
-    {
-        id: "fLroW6",
-        category: "Islamic Studies", // Change category
-        image: course_images.python_3,
-        course_name: "Islamic Art and Architecture",
-        description: "Explore the rich heritage of Islamic art and architecture across different cultures and time periods.",
-        rating_count: 1844,
-        rating_star: 4.3,
-        students: 7330,
-        creator: "Islamic Art Historian",
-        updated_date: "5/2022",
-        lang: "English",
-        actual_price: 84.99,
-        discounted_price: 9.99,
-        what_you_will_learn: [
-            "Discover Islamic Art Styles",
-            "Explore Islamic Architecture",
-            "Understand Calligraphy and Manuscripts"
-        ],
-        content: [
-            "Introduction to Islamic Art",
-            "Islamic Calligraphy",
-            "Islamic Manuscripts",
-            "Islamic Architecture",
-            "Islamic Art Across Cultures",
-            "Islamic Art Conservation"
-        ]
-    },
-
-    
 ];
+
   
  //images 
 async function fetchPexelsImageUrl(searchQuery) {
