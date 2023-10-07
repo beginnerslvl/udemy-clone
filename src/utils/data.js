3262972965e78724523f5f7ae3a833fb3d250861
Waleed Ajmal <127334056+Majboor@users.noreply.github.com>
2023-10-07 19:23:49 +0500

    Update data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,8 +1,70 @@
+import { course_images } from "./images";
+
+
 const courses = [
+    
+    {
+        id: "xMHCZl",
+        category: "Islamic Studies", // Change category
+        image: course_images.python_5,
+        course_name: "Islamic Studies: The Comprehensive Islamic Course",
+        description: "Learn A-Z everything about Islamic Studies, from the basics, to advanced topics in Quran, Hadith, and more!",
+        rating_count: 3059,
+        rating_star: 4.4,
+        students: 18430,
+        creator: "Islamic Scholar Institute",
+        updated_date: "9/2015",
+        lang: "English",
+        actual_price: 84.99,
+        discounted_price: 9.99,
+        what_you_will_learn: [
+            "Study Islamic Theology",
+            "Become Proficient in Quranic Recitation",
+            "Understand Hadith Interpretation"
+        ],
+        content: [
+            "Introduction to Islamic Studies",
+            "The Quran and Its Sciences",
+            "Hadith and Its Importance",
+            "Islamic Jurisprudence",
+            "Islamic History",
+            "Islamic Ethics",
+            "Contemporary Issues in Islam"
+        ]
+    },
+    {
+        id: "dtfo9e",
+        category: "Islamic Studies", // Change category
+        image: "https://images.pexels.com/photos/6281718/pexels-photo-6281718.jpeg",
+        course_name: "Islamic Finance and Banking",
+        description: "Learn about Islamic finance principles and banking practices in accordance with Shariah law.",
+        rating_count: 3059,
+        rating_star: 4.3,
+        students: 184053,
+        creator: "Islamic Finance Expert",
+        updated_date: "9/2019",
+        lang: "English",
+        actual_price: 84.99,
+        discounted_price: 9.99,
+        what_you_will_learn: [
+            "Understand the Principles of Islamic Finance",
+            "Explore Shariah-Compliant Investment",
+            "Learn Islamic Banking Practices",
+            "Evaluate Islamic Insurance (Takaful)"
+        ],
+        content: [
+            "Introduction to Islamic Finance",
+            "Islamic Banking Products",
+            "Islamic Investment Principles",
+            "Islamic Insurance (Takaful)",
+            "Islamic Financial Markets",
+            "Islamic Finance Case Studies"
+        ]
+    },
     {
         id: "fLroW6",
         category: "Islamic Studies", // Change category
-        
+        image: course_images.python_3,
         course_name: "Islamic Art and Architecture",
         description: "Explore the rich heritage of Islamic art and architecture across different cultures and time periods.",
         rating_count: 1844,
@@ -25,11 +87,43 @@ const courses = [
             "Islamic Architecture",
             "Islamic Art Across Cultures",
             "Islamic Art Conservation"
+        ]
+    },
+    
+
+    {
+        id: "dtfo9e",
+        category: "Islamic History", // Change category
+        image: course_images.python_4,
+        course_name: "Islamic Finance and Banking",
+        description: "Learn about Islamic finance principles and banking practices in accordance with Shariah law.",
+        rating_count: 3059,
+        rating_star: 4.3,
+        students: 184053,
+        creator: "Islamic Finance Expert",
+        updated_date: "9/2019",
+        lang: "English",
+        actual_price: 84.99,
+        discounted_price: 9.99,
+        what_you_will_learn: [
+            "Understand the Principles of Islamic Finance",
+            "Explore Shariah-Compliant Investment",
+            "Learn Islamic Banking Practices",
+            "Evaluate Islamic Insurance (Takaful)"
+        ],
+        content: [
+            "Introduction to Islamic Finance",
+            "Islamic Banking Products",
+            "Islamic Investment Principles",
+            "Islamic Insurance (Takaful)",
+            "Islamic Financial Markets",
+            "Islamic Finance Case Studies"
         ]
     },
     {
-        id: "fTroW6",
-        category: "Islamic Sstudies", // Change category        
+        id: "fLroW6",
+        category: "Islamic Studies", // Change category
+        image: course_images.python_3,
         course_name: "Islamic Art and Architecture",
         description: "Explore the rich heritage of Islamic art and architecture across different cultures and time periods.",
         rating_count: 1844,
@@ -54,45 +148,13 @@ const courses = [
             "Islamic Art Conservation"
         ]
     },
-    {
-        id: "fLRroW6",
-        category: "Islamic Studies", // Change category        
-        course_name: "Islamic Art and Architecture",
-        description: "Explore the rich heritage of Islamic art and architecture across different cultures and time periods.",
-        rating_count: 1844,
-        rating_star: 4.3,
-        students: 7330,
-        creator: "Islamic Art Historian",
-        updated_date: "5/2022",
-        lang: "English",
-        actual_price: 84.99,
-        discounted_price: 9.99,
-        what_you_will_learn: [
-            "Discover Islamic Art Styles",
-            "Explore Islamic Architecture",
-            "Understand Calligraphy and Manuscripts"
-        ],
-        content: [
-            "Introduction to Islamic Art",
-            "Islamic Calligraphy",
-            "Islamic Manuscripts",
-            "Islamic Architecture",
-            "Islamic Art Across Cultures",
-            "Islamic Art Conservation"
-        ]
-    },
-  // Add more courses here
+
+    
 ];
 
-// Use the function to update the 'image' field for each course
-(async () => {
-  for (const course of courses) {
-    course.image = await getImageUrlForCourse(course.course_name);
-  }
-})();
+  
 
 export default courses;
-
 export const PYTHON = "Islamic Studies"; // Change category names as needed
 export const WEB_DEVELOPMENT = "Islamic History";
 export const DATA_SCIENCE = "Quranic Studies";
