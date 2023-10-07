ae4f251fe363777fe4afcda058276f9abe529694
Waleed Ajmal <127334056+Majboor@users.noreply.github.com>
2023-10-07 16:36:24 +0500

    remove dupliacte config

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { other_images } from "../utils/images";
-import config from '.../utils/data';
 import config from '.../utils/config';
 const herotext = config.navbarItems.map(item => item.label);
 
