02e99f1a88f46efe3e3c35f400f99658406c37f6
Waleed Ajmal <127334056+Majboor@users.noreply.github.com>
2023-10-07 20:53:53 +0500

    update courses and api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,34 +1,47 @@
-export function fetchPexelsImageUrl(searchQuery) {
+async function fetchPexelsImageUrl(searchQuery) {
     const apiKey = 'sHlEMO77bq1Ir6j0GC1tpmwbBPbTt9TXbJzdfLYzPkdeQLWjvTRWT5hM';
-
+  
     const headers = {
-        Authorization: apiKey,
+      'Authorization': apiKey,
     };
-
-    const apiUrl = `https://api.pexels.com/v1/search?query=${searchQuery}`;
-
-    return new Promise(async (resolve, reject) => {
-        try {
-            const response = await fetch(apiUrl, {
-                headers,
-            });
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-
-            const data = await response.json();
-            const photos = data.photos || [];
-
-            if (photos.length > 0) {
-                const firstPhoto = photos[0];
-                const imageUrl = firstPhoto.src.original;
-                resolve(imageUrl); // Resolve with the image URL
-            } else {
-                reject(new Error('No images found for the query.'));
-            }
-        } catch (error) {
-            reject(error);
-        }
-    });
-}
+  
+    const url = `https://api.pexels.com/v1/search?query=${searchQuery}`;
+  
+    try {
+      const response = await fetch(url, { headers });
+      if (!response.ok) {
+        throw new Error(`Request failed with status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      const photos = data.photos || [];
+  
+      if (photos.length > 0) {
+        const firstPhoto = photos[0];
+        const imageUrl = firstPhoto.src.original;
+  
+        return imageUrl; // Return the fetched image URL
+      } else {
+        console.log('No images found for the query.');
+        return null;
+      }
+    } catch (error) {
+      console.error(`Error fetching image: ${error.message}`);
+      return null;
+    }
+  }
+  
+  // Loop through courses and update the image property
+  async function updateCourseImages() {
+    for (let i = 0; i < courses.length; i++) {
+      const course = courses[i];
+      const searchQuery = course.course_name;
+  
+      const imageUrl = await fetchPexelsImageUrl(searchQuery);
+  
+      if (imageUrl) {
+        course.image = imageUrl;
+        console.log(`Updated image for course: ${course.course_name}`);
+      }
+    }
+  }
