0b5cd931b476241bbdede1ad588443d12efc52a4
Waleed Ajmal <127334056+Majboor@users.noreply.github.com>
2023-10-08 16:40:51 +0500

    update

--- src/utils/data.js
+++ src/utils/data.js
@@ -150,24 +150,7 @@ async function fetchPexelsImageUrl(searchQuery) {
   }
   updateCourseImages();  
 
-  const config = {
-    // Navigation Bar
+
   
-  
-    // Hero Section
-    heroItems: [
-      { label: 'Learning for all' },
-      { label: 'Huge sale on courses from $9.99. Ends Aug. 31.' },
-      { label: 'Thousands of courses to help you succeed in your goals at work and            in life.' },
-    ],
-  
-    // Design Color Scheme
-    design: [
-      { label: '#FF5733' },    // primaryColor (bright orange)
-      { label: 'white' },      // primaryColor1
-      { label: '#212121' },    // primaryColor2 (dark gray)
-    ],
-  };
-  
-export { config, courses };
+export default courses;
 
